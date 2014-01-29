bf01e1baa70fe834ffa3f2b3d8dec9a5e512d9cf
Graham Ollis <perl@wdlabs.com>
2014-01-29 09:09:55 -0500

    broekn attempt to use static

--- inc/Archive/Ar/Libarchive/ModuleBuild.pm
+++ inc/Archive/Ar/Libarchive/ModuleBuild.pm
@@ -3,6 +3,8 @@ package Archive::Ar::Libarchive::ModuleBuild;
 use strict;
 use warnings;
 use Alien::Libarchive;
+use ExtUtils::CChecker;
+use Text::ParseWords qw( shellwords );
 use base qw( Module::Build );
 
 my $alien;
@@ -20,6 +22,32 @@ sub new
   {
     $cflags .= ' -DLIBARCHIVE_STATIC';
     $libs =~ s/-larchive\b/-larchive_static/;
+  }
+
+  my $cc = ExtUtils::CChecker->new;
+  $cc->push_extra_compiler_flags(shellwords $cflags);
+  $cc->push_extra_linker_flags(shellwords $libs);
+  
+  my $ctest = "#include <archive.h>\n" .
+              "int main(int argc, char *argv[]) {\n" .
+              "  struct archive *a = archive_read_new();\n" .
+              "  return 0;\n" .
+              "}\n";
+  
+  my $ok = $cc->try_compile_run(
+    source => $ctest,
+  );
+  
+  unless($ok)
+  {
+    $libs = "-Wl,-Bstatic $libs -Wl,-Bdynamic";
+    my $cc = ExtUtils::CChecker->new;
+    $cc->push_extra_compiler_flags(shellwords $cflags);
+    $cc->push_extra_linker_flags(shellwords($libs));
+    $cc->assert_compile_run(
+      diag => 'unable to link against libarchive',
+      source => $ctest,
+    );
   }
   
   $args{extra_compiler_flags} = $cflags;
